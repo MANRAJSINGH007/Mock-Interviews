6172e285196ccaa3aff945892590029fced2fa19
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2019-11-05 20:29:07 +0530

    Create Interview Shuriken 32: Parenthesis Matching.java

---
+++ Interview Shuriken 32: Parenthesis Matching.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static boolean check(String s, int n, int i, int opening){
+        if(i == n){
+            if(opening == 0) return true;
+            return false;
+        }
+        
+        char curr = s.charAt(i);
+        
+        if(opening == 0){
+            if(curr == ')') return false;
+            return check(s, n, i + 1, opening + 1);
+        }
+        
+        else if(s.charAt(i) == '('){
+            return check(s, n, i + 1, opening + 1);
+        }
+        
+        return check(s, n, i + 1, opening) || check(s, n, i + 1, opening + 1) || check(s, n, i + 1, opening - 1);
+    }
+    
+	public static void main(String[] args) {
+		/* Your class should be named Main.
+ 			* Read input as specified in the question.
+ 			* Print output as specified in the question.
+		*/
+
+		// Write your code here
+        Scanner sc = new Scanner(System.in);
+        
+        String s = sc.next();
+        
+        System.out.println(check(s, s.length(), 0, 0));
+
+	}
+
+}
